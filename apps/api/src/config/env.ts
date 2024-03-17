3dbc6647d1ceb083e52fc4f63ff256240d8b47b5
ibrahim <ibrahimyaacob92@yahoo.com>
2024-03-18 07:01:33 +0800

    remove host

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -3,7 +3,6 @@ import z from "zod";
 export const envSchema = z.object({
   PORT: z.coerce.number().int().default(5000),
   NODE_ENV: z.string().default("development"),
-  HOST: z.string().default("localhost"),
 });
 
 export const env = envSchema.parse(process.env);
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -26,29 +26,14 @@ app.register(cors, {
 
 app.register(helmet);
 
-if (env.HOST) {
-  app.listen(
-    {
-      port: env.PORT,
-      host: env.HOST,
-    },
-    (err, _address) => {
-      if (err) {
-        app.log.error(err);
-        process.exit(1);
-      }
+app.listen(
+  {
+    port: env.PORT,
+  },
+  (err, _address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
     }
-  );
-} else {
-  app.listen(
-    {
-      port: env.PORT,
-    },
-    (err, _address) => {
-      if (err) {
-        app.log.error(err);
-        process.exit(1);
-      }
-    }
-  );
-}
+  }
+);
